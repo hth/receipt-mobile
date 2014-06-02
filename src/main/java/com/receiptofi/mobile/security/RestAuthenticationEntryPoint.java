b4972abff17082b4f5ae69562b82d65995a0d77e
Hitender Tholia <hitender@tholix.com>
2014-06-01 21:09:52 -0700

    working REST test

---
+++ src/main/java/com/receiptofi/mobile/security/RestAuthenticationEntryPoint.java
@@ -0,0 +1,26 @@
+package com.receiptofi.mobile.security;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+/**
+ * User: hitender
+ * Date: 6/1/14 6:22 PM
+ */
+@Component("restAuthenticationEntryPoint")
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException
+    ) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+}
