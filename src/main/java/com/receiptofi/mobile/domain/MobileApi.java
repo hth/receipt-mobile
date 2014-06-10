99b956b45088382cda44ea9da9ddffb554db3f2e
Hitender Tholia <hitender@tholix.com>
2014-06-09 19:17:47 -0700

    added Jackson annotation; for can live without it

--- src/main/java/com/receiptofi/mobile/domain/MobileApi.java
+++ src/main/java/com/receiptofi/mobile/domain/MobileApi.java
@@ -1,11 +1,17 @@
 package com.receiptofi.mobile.domain;
 
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.annotate.JsonProperty;
+
 /**
  * User: hitender
  * Date: 6/9/14 12:37 PM
  */
+@JsonIgnoreProperties(ignoreUnknown=true)
 public final class MobileApi {
-    boolean working;
+
+    @JsonProperty("working")
+    private boolean working;
 
     private MobileApi(boolean working) {
         this.working = working;
--- src/main/java/com/receiptofi/mobile/domain/UserAccess.java
+++ src/main/java/com/receiptofi/mobile/domain/UserAccess.java
@@ -1,10 +1,16 @@
 package com.receiptofi.mobile.domain;
 
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.annotate.JsonProperty;
+
 /**
  * User: hitender
  * Date: 6/9/14 12:06 PM
  */
+@JsonIgnoreProperties(ignoreUnknown=true)
 public final class UserAccess {
+
+    @JsonProperty("working")
     private String access;
 
     private UserAccess(String access) {
