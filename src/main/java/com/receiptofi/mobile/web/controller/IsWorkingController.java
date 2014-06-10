fc0fcb633eb04c715019b6f2cfb23eac381505b1
Hitender Tholia <hitender@tholix.com>
2014-06-09 18:42:57 -0700

    support only JSON call

--- src/main/java/com/receiptofi/mobile/web/controller/IsWorkingController.java
+++ src/main/java/com/receiptofi/mobile/web/controller/IsWorkingController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,21 +23,35 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public final class IsWorkingController {
     private static final Logger log = LoggerFactory.getLogger(IsWorkingController.class);
 
-    @RequestMapping(value = "/isWorking", method = RequestMethod.GET)
+    /**
+     * Supports HTML call
+     * @return
+     */
+    @RequestMapping(
+            value = "/isWorking",
+            method = RequestMethod.GET,
+            produces = {
+                    MediaType.TEXT_HTML_VALUE + "; charset=utf-8",
+            }
+    )
+    @ResponseStatus(HttpStatus.OK)
     public String isWorking() {
         log.info("isWorking");
         return "isWorking";
     }
 
-//    @ResponseStatus(HttpStatus.OK)
+    /**
+     * Supports JSON call
+     * @return
+     */
     @RequestMapping(
             value = "/healthCheck",
             method = RequestMethod.GET,
-            produces = "application/json; charset=utf-8"
+            produces = MediaType.APPLICATION_JSON_VALUE + "; charset=utf-8"
     )
     public @ResponseBody
     MobileApi healthCheck() {
-        //TODO should perform some kind of health check
+        //TODO should perform some kind of health check like connecting to mongo
         return MobileApi.newInstance(true);
     }
 }
