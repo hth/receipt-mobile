ab4501a6f1188e4ee45d6838f6a52ddc73316ae7
Hitender Tholia <tholiahitender@gmail.com>
2014-12-22 02:56:47 -0800

    removed unused import

--- web/src/main/java/com/receiptofi/mobile/web/controller/api/ReceiptItemsController.java
+++ web/src/main/java/com/receiptofi/mobile/web/controller/api/ReceiptItemsController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
