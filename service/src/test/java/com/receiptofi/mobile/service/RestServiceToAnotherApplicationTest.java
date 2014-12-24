ac27031afb96285210249aa83032627ba12ddf43
Hitender Tholia <tholiahitender@gmail.com>
2014-12-24 00:23:08 -0800

    PMD fix

--- service/src/test/java/com/receiptofi/mobile/service/RestServiceToAnotherApplicationTest.java
+++ service/src/test/java/com/receiptofi/mobile/service/RestServiceToAnotherApplicationTest.java
@@ -1,6 +1,6 @@
 package com.receiptofi.mobile.service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Before;
 import org.junit.Test;
--- web/src/test/java/com/receiptofi/mobile/web/controller/api/DeviceControllerTest.java
+++ web/src/test/java/com/receiptofi/mobile/web/controller/api/DeviceControllerTest.java
@@ -1,8 +1,13 @@
 package com.receiptofi.mobile.web.controller.api;
 
 import static com.receiptofi.mobile.util.MobileSystemErrorCodeEnum.USER_INPUT;
-import static com.receiptofi.mobile.web.controller.AccountControllerTest.*;
-import static org.junit.Assert.*;
+import static com.receiptofi.mobile.web.controller.AccountControllerTest.ERROR;
+import static com.receiptofi.mobile.web.controller.AccountControllerTest.REASON;
+import static com.receiptofi.mobile.web.controller.AccountControllerTest.SYSTEM_ERROR;
+import static com.receiptofi.mobile.web.controller.AccountControllerTest.SYSTEM_ERROR_CODE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.never;
@@ -16,8 +21,6 @@ import com.google.gson.JsonParser;
 import com.receiptofi.mobile.domain.AvailableAccountUpdates;
 import com.receiptofi.mobile.service.AuthenticateService;
 import com.receiptofi.mobile.service.DeviceService;
-import com.receiptofi.mobile.service.MobileAccountService;
-import com.receiptofi.mobile.web.controller.AccountControllerTest;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -26,7 +29,6 @@ import org.mockito.MockitoAnnotations;
 
 import java.io.IOException;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings ({
@@ -81,7 +83,7 @@ public class DeviceControllerTest {
     @Test
     public void testRegisterDeviceFailsToFindUser() throws IOException {
         when(authenticateService.getReceiptUserId(anyString(), anyString())).thenReturn(null);
-        assertNull(deviceController.hasUpdate("", "", "did", httpServletResponse));
+        assertNull(deviceController.registerDevice("", "", "did", httpServletResponse));
     }
 
     @Test
