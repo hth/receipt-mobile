97fd8d579d44cc2d07cc8c6295c891adf835602c
Hitender Tholia <tholiahitender@gmail.com>
2015-04-09 16:18:11 -0700

    added ExpenseTag service for mobile

---
+++ service/src/main/java/com/receiptofi/mobile/service/ExpenseTagMobileService.java
@@ -0,0 +1,34 @@
+package com.receiptofi.mobile.service;
+
+import com.receiptofi.domain.ExpenseTagEntity;
+import com.receiptofi.repository.ExpenseTagManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * User: hitender
+ * Date: 4/9/15 4:00 PM
+ */
+@SuppressWarnings ({
+        "PMD.BeanMembersShouldSerialize",
+        "PMD.LocalVariableCouldBeFinal",
+        "PMD.MethodArgumentCouldBeFinal",
+        "PMD.LongVariable"
+})
+@Service
+public class ExpenseTagMobileService {
+
+    private ExpenseTagManager expenseTagManager;
+
+    @Autowired
+    public ExpenseTagMobileService(ExpenseTagManager expenseTagManager) {
+        this.expenseTagManager = expenseTagManager;
+    }
+
+    public List<ExpenseTagEntity> getExpenseTags(String rid) {
+        return expenseTagManager.getExpenseTags(rid);
+    }
+}
