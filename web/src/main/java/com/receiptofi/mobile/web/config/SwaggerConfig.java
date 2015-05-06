f8b3a5679ba3057d6016baf27210b64638abca3b
Hitender Tholia <tholiahitender@gmail.com>
2015-05-06 04:07:28 -0700

    upgraded to swagger 2.0

--- web/src/main/java/com/receiptofi/mobile/web/config/SwaggerConfig.java
+++ web/src/main/java/com/receiptofi/mobile/web/config/SwaggerConfig.java
@@ -1,58 +1,36 @@
 package com.receiptofi.mobile.web.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
-import com.mangofactory.swagger.models.dto.ApiInfo;
-import com.mangofactory.swagger.plugin.EnableSwagger;
-import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * User: hitender
  * Date: 12/9/14 1:45 AM
  */
 @Configuration
-@EnableWebMvc
-@ComponentScan("com.receiptofi.mobile.web.controller")
-@EnableSwagger /* Loads the spring beans required by the framework. */
-public class SwaggerConfig extends WebMvcConfigurerAdapter {
-    private SpringSwaggerConfig springSwaggerConfig;
+@EnableSwagger2 //Loads the spring beans required by the framework
+public class SwaggerConfig {
 
-    /**
-     * Required to autowire SpringSwaggerConfig.
-     */
-    @Autowired
-    public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
-        this.springSwaggerConfig = springSwaggerConfig;
-    }
-
-    /**
-     * Every SwaggerSpringMvcPlugin bean is picked up by the swagger-mvc framework - allowing for multiple
-     * swagger groups i.e. same code base multiple swagger resource listings.
-     * Include patterns from root - .*
-     * Include specific pattern /api/* - .api.*
-     */
     @Bean
-    public SwaggerSpringMvcPlugin customImplementation() {
-        return new SwaggerSpringMvcPlugin(this.springSwaggerConfig)
-                .includePatterns(".*");
+    public Docket petApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .build()
+                .apiInfo(apiInfo());
     }
 
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
+    //.useDefaultResponseMessages(false).pathMapping("/rest")
 
     private ApiInfo apiInfo() {
         ApiInfo apiInfo = new ApiInfo(
                 "My Apps API Title",
                 "My Apps API Description",
+                "My Apps API Version",
                 "My Apps API terms of service",
                 "My Apps API Contact Email",
                 "My Apps API Licence Type",
